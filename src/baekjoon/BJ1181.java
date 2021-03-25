24ee295b7a08000d428341ce260ff8fd77c9b690
kijk6010 <kijk6010@naver.com>
2021-03-25 23:39:00 +0900

    BufferReader사용 입력_단어정렬(시간단축)

--- src/baekjoon/BJ1181.java
+++ src/baekjoon/BJ1181.java
@@ -1,16 +1,18 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class BJ1181 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		String[] arr= new String[N];
 		for(int i=0;i<N;i++) {
-			arr[i]=sc.next();
+			arr[i]=br.readLine();
 		}
 		Arrays.sort(arr, new Comparator<String>() {
 			@Override
