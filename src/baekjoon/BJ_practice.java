02771b217e331a3eea7c83c40eb61885c2071914
kijk6010 <kijk6010@naver.com>
2021-03-26 11:00:56 +0900

    ATM_BJ11399(그리디)

--- src/baekjoon/BJ_practice.java
+++ src/baekjoon/BJ_practice.java
@@ -6,29 +6,18 @@ public class BJ_practice {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N=sc.nextInt();
-		int[][] arr = new int[N][2];
+		int[] arr= new int[N];
 		for(int i=0;i<N;i++) {
-			arr[i][0]=sc.nextInt();
-			arr[i][1]=sc.nextInt();
+			arr[i]=sc.nextInt();
 		}
-		Arrays.sort(arr,new Comparator<int[]>(){
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[1]==o2[1]) {
-					return o1[0]-o2[1];
-				}else {
-					return o1[1]-o2[1];	
-				}
-			}
-		});
-		int end=0;
-		int cnt=0;
-		for(int i=0;i<N;i++) {
-			if(arr[i][0]>=end) {
-				cnt++;
-				end=arr[i][1];
-			}
+		Arrays.sort(arr);
+		for(int i=1;i<N;i++) {
+			arr[i]+=arr[i-1];
 		}
-		System.out.print(cnt);
+		int result =0;
+		for(int j:arr) {
+			result+=j;
+		}
+		System.out.print(result);
 	}
 }
